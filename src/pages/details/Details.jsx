0f1ee18c943c6c1006cebd880e22a7e722e8ff69
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2024-01-06 22:27:12 +0530

    Home section done

---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.scss"
+import useFetch from '../../hooks/useFetch.jsx'
+
+const Details = () => {
+  
+
+  return (
+    <div>details</div>
+  )
+}
+
+export default Details
